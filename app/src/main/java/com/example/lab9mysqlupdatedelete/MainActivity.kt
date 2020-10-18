0519ebba3e2e3b60e8200e70471bcce1eb7b26b6
sunidsa <meen_sunidsa@kkumail.com>
2020-10-18 21:53:31 +0700

    ASS09_Sec1_613020177-6

---
+++ app/src/main/java/com/example/lab9mysqlupdatedelete/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.lab9mysqlupdatedelete
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_edit_delete.*
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.Call
+import okhttp3.Response
+import javax.security.auth.callback.Callback
+
+class MainActivity : AppCompatActivity() {
+    var studentList = arrayListOf<Student>()
+    val createClient = StudentAPI.create()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recycler_view.layoutManager = LinearLayoutManager(applicationContext) as RecyclerView.LayoutManager?
+        recycler_view.itemAnimator = DefaultItemAnimator() as RecyclerView.ItemAnimator?
+        recycler_view.addItemDecoration(DividerItemDecoration(recycler_view.getContext(), DividerItemDecoration.VERTICAL))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        callStudentData()
+    }
+    fun clickSearch(v:View){
+        studentList.clear();
+        if(edt_search.text.isEmpty()){
+            callStudentData()
+        }else{
+            createClient.retrieveStudentID(edt_search.text.toString())
+                .enqueue(object : retrofit2.Callback<Student> {
+
+                    override fun onResponse(call: retrofit2.Call<Student>, response: retrofit2.Response<Student>){
+                        studentList.add(Student(response.body()?.std_id.toString(),response.body()?.std_name.toString(),
+                            response.body()?.std_age.toString().toInt()))
+
+                        recycler_view.adapter = EditStudentsAdapter(studentList,applicationContext)
+                    }
+                    override fun onFailure(call: retrofit2.Call<Student>, t: Throwable) = t.printStackTrace()
+                })
+        }
+
+    }
+
+    fun callStudentData(){
+        studentList.clear();
+        createClient.
+            retrieveStudent()
+                .enqueue(object : retrofit2.Callback<List<Student>> {
+                    override fun onResponse(call: retrofit2.Call<List<Student>>, response: retrofit2.Response<List<Student>>) {
+                        response.body()?.forEach {
+                            studentList.add(Student(it.std_id, it.std_name, it.std_age))
+                        }
+                        recycler_view.adapter = EditStudentsAdapter(studentList, applicationContext)
+                    }
+
+                    override fun onFailure(call: retrofit2.Call<List<Student>>, t: Throwable) =
+                        t.printStackTrace()
+                })
+
+    }
+}
+
